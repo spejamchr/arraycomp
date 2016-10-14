5ad4bb82b6f45c61f722cf46a88a5374a3b0ca09
Spencer Christiansen <spencer@trooptrack.com>
2016-10-14 09:46:17 -0600

    Use Google charts, it's free. Highcharts requires a licence

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -15,5 +15,4 @@
 //= require jquery_ujs
 //= require turbolinks
 //= require_tree .
-//= require highcharts
 //= require chartkick
--- app/helpers/charts_helper.rb
+++ app/helpers/charts_helper.rb
@@ -1,17 +1,13 @@
 module ChartsHelper
   def all_comp_arrays
-    line_chart all_comp_arrays_charts_path, height: '500px', library: {
-      title: {text: 'All Arrays', x: -20},
-      yAxis: {
-         title: {
-             text: 'Value (USD)'
-         }
-      },
-      xAxis: {
-         title: {
-             text: 'Date'
-         }
-      }
-    }
+    line_chart all_comp_arrays_charts_path,
+               xtitle: 'Date',
+               ytitle: 'Value (USD)', library: {
+                 interpolateNulls: true,
+                 pointSize: 0,
+                 curveType: 'none',
+                 title: 'All Arrays',
+                 legend: 'bottom'
+               }
   end
 end
